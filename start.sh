45f4952e27dc8089e4ebcad2c980cb2dc3cb0ef0
Jarle Fosen <jarlefosen@gmail.com>
2015-09-19 17:58:14 +0200

    Fixed bower install in package.json

--- start.sh
+++ start.sh
@@ -7,4 +7,5 @@ echo "Clone repository from branc $BRANCH"
 git clone --quiet https://github.com/jarlefosen/dagen_social_feed.git --branch $BRANCH $APP_LOCATION
 
 cd $APP_LOCATION
+npm install
 npm start
